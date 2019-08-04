76146ad5a9b858ee546409d65ecb0c0c0abccb42
Chris Martin <ch.martin@gmail.com>
2019-08-04 14:48:11 -0600

    add Compose to Prelude

--- dsv/library/Dsv/Prelude.hs
+++ dsv/library/Dsv/Prelude.hs
@@ -5,3 +5,4 @@ module Dsv.Prelude (module Prelude) where
 import Prelude hiding ((.), id)
 import Control.Category as Prelude ((.), id)
 import Data.Coerce as Prelude
+import Data.Functor.Compose as Prelude (Compose (Compose))
