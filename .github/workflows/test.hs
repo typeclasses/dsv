4c685351041b3066bc41f1da388de2b6e48d8fc6
Chris Martin <ch.martin@gmail.com>
2021-03-08 23:58:46 -0700

    remove doctest constraints from CI

--- .github/workflows/test.hs
+++ .github/workflows/test.hs
@@ -5,11 +5,9 @@ main =
   getEnv "ghc" >>= \ghc ->
     case ghc of
       "8.6.5" -> callProcess "cabal" ["test", "dsv"
-                  , "--constraint=doctest == 0.16.*"
                   , "--constraint=template-haskell == 2.14.*"
                   ]
       "8.8.4" -> callProcess "cabal" ["test", "dsv"]
       "8.10.2" -> callProcess "cabal" ["test", "dsv"
-                  , "--constraint=doctest == 0.17.*"
                   , "--constraint=template-haskell == 2.16.*"
                   ]
