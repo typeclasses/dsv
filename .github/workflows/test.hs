fe1bb476aef41a73541c0d1525de3a146b6845f9
Chris Martin <ch.martin@gmail.com>
2021-03-09 12:17:07 -0700

    test with ghc 9.0

--- .github/workflows/test.hs
+++ .github/workflows/test.hs
@@ -4,12 +4,22 @@ import System.Process
 main =
   getEnv "ghc" >>= \ghc ->
     case ghc of
-      "8.6.5" -> callProcess "cabal" ["test", "dsv"
+      "8.6.5"  -> callProcess "cabal" ["test", "dsv"
                   , "--constraint=bytestring == 0.10.*"
                   , "--constraint=template-haskell == 2.14.*"
                   ]
-      "8.8.4" -> callProcess "cabal" ["test", "dsv"]
+
+      "8.8.4"  -> callProcess "cabal" ["test", "dsv"]
+
       "8.10.2" -> callProcess "cabal" ["test", "dsv"
                   , "--constraint=bytestring == 0.11.*"
                   , "--constraint=template-haskell == 2.16.*"
+                  ]
+
+      -- Currently building but not testing because
+      -- hedgehog does not support GHC 9.0.
+      -- https://github.com/hedgehogqa/haskell-hedgehog/issues/416
+      "9.0.1"  -> callProcess "cabal" ["build", "dsv"
+                  , "--constraint=bytestring == 0.11.*"
+                  , "--constraint=template-haskell == 2.17.*"
                   ]
