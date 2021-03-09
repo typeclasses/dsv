593e0f7dc1fdd01aefc246b2450b031b24ab2e92
Chris Martin <ch.martin@gmail.com>
2021-03-09 12:13:11 -0700

    support bytestring 0.11

--- .github/workflows/test.hs
+++ .github/workflows/test.hs
@@ -5,9 +5,11 @@ main =
   getEnv "ghc" >>= \ghc ->
     case ghc of
       "8.6.5" -> callProcess "cabal" ["test", "dsv"
+                  , "--constraint=bytestring == 0.10.*"
                   , "--constraint=template-haskell == 2.14.*"
                   ]
       "8.8.4" -> callProcess "cabal" ["test", "dsv"]
       "8.10.2" -> callProcess "cabal" ["test", "dsv"
+                  , "--constraint=bytestring == 0.11.*"
                   , "--constraint=template-haskell == 2.16.*"
                   ]
